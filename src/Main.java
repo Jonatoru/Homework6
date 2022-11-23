ab4bc9f87ff8169154c79706d77a82488d52c15f
Jonatoru <jetrence95@gmail.com>
2022-11-24 00:04:24 +0500

    Сделал задачу 2.

--- src/Main.java
+++ src/Main.java
@@ -7,5 +7,11 @@ public class Main {
         for (int i = 1; i <= 10; i++) {
             System.out.println("Считаю: " + i);
         }
+
+        System.out.println("Задача 2");
+
+        for (int i = 10; i >= 1; i--) {
+            System.out.println("Считаю: " + i);
+        }
     }
 }
